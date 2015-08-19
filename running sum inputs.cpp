f6920f4828a1e060dfd9a62c3087c77c98c2b761
Rose <aerwings@gmail.com>
2015-08-18 23:08:52 -0400

    Create running sum inputs.cpp

---
+++ running sum inputs.cpp
@@ -0,0 +1,14 @@
+// Write a program that computes a running sum of inputs from the user, terminating when the user gives an input value of zero.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+do {
+    cout << "Please enter a number." << endl;
+    int inputnum;
+    cin >> inputnum;
+    cout << inputnum
+    
+    
+// OHGOD WHY
