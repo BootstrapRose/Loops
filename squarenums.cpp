187c2490e751c02d3e63d619625b213dc9c7f7ed
Rose <aerwings@gmail.com>
2015-08-20 22:14:49 -0400

    Update squarenums.cpp

--- squarenums.cpp
+++ squarenums.cpp
@@ -1,1 +1,12 @@
 // Write a program that dislays the first 20 square numbers.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  for (i=1; i<=20; i++){
+    cout << i * i << endl;
+  }
+  
+  return 0;
+}
